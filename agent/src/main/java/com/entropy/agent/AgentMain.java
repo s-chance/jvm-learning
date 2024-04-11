40ceeb8fb726498f5e6224e36d6b007f68523736
s-chance <entropy.tree@outlook.com>
2024-04-11 17:17:09 +0800

    feat(apm): application performance monitor demo

--- agent/src/main/java/com/entropy/agent/AgentMain.java
+++ agent/src/main/java/com/entropy/agent/AgentMain.java
@@ -3,6 +3,11 @@ package com.entropy.agent;
 import com.entropy.agent.command.ClassCommand;
 import com.entropy.agent.command.MemoryCommand;
 import com.entropy.agent.command.ThreadCommand;
+import com.entropy.agent.enhancer.AgentParam;
+import com.entropy.agent.enhancer.TimingAdvice;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.matcher.ElementMatchers;
 
 import java.lang.instrument.Instrumentation;
 import java.util.Scanner;
@@ -10,7 +15,24 @@ import java.util.Scanner;
 public class AgentMain {
     // premain方法
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("premain执行了....");
+        // 使用bytebuddy增强类
+        new AgentBuilder.Default()
+                // 禁止bytebuddy处理时修改类名
+                .disableClassFormatChanges()
+                // 处理时使用retransform增强
+                .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+                // 打印错误日志
+                .with(new AgentBuilder.Listener.WithTransformationsOnly(AgentBuilder.Listener.StreamWriting.toSystemOut()))
+                // 匹配哪些类
+                .type(ElementMatchers.isAnnotatedWith(ElementMatchers.named("org.springframework.web.bind.annotation.RestController")
+                        .or(ElementMatchers.named("org.springframework.web.bind.annotation.Controller"))))
+                // 增强，使用MyAdvice通知，对所有方法都进行增强
+                .transform((builder, typeDescription, classLoader, javaModule, protectionDomain) ->
+                        builder.visit(Advice
+                                        .withCustomMapping()
+                                        .bind(AgentParam.class, agentArgs)
+                                .to(TimingAdvice.class).on(ElementMatchers.any())))
+                .installOn(inst);
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
