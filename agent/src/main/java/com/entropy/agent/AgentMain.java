648247a205f71dcf762faa2b0b92480cad6d7755
s-chance <entropy.tree@outlook.com>
2024-04-11 15:08:15 +0800

    feat(*): add menu of agentmain

--- agent/src/main/java/com/entropy/agent/AgentMain.java
+++ agent/src/main/java/com/entropy/agent/AgentMain.java
@@ -5,6 +5,7 @@ import com.entropy.agent.command.MemoryCommand;
 import com.entropy.agent.command.ThreadCommand;
 
 import java.lang.instrument.Instrumentation;
+import java.util.Scanner;
 
 public class AgentMain {
     // premain方法
@@ -13,11 +14,28 @@ public class AgentMain {
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
-        MemoryCommand.printMemory();
-        MemoryCommand.heapDump();
-        ThreadCommand.printThreadInfo();
-        ClassCommand.printAllClassLoader(inst);
-        ClassCommand.printClassSourceCode(inst);
-        ClassCommand.enhanceClass(inst);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("菜单:\n" +
+                    "1.查看内存使用情况\n" +
+                    "2.生成堆内存快照\n" +
+                    "3.打印栈信息\n" +
+                    "4.打印类加载器\n" +
+                    "5.打印类源码\n" +
+                    "6.打印方法的参数和耗时\n" +
+                    "7.退出\n");
+            String input = scanner.next();
+            switch (input) {
+                case "1" -> MemoryCommand.printMemory();
+                case "2" -> MemoryCommand.heapDump();
+                case "3" -> ThreadCommand.printThreadInfo();
+                case "4" -> ClassCommand.printAllClassLoader(inst);
+                case "5" -> ClassCommand.printClassSourceCode(inst);
+                case "6" -> ClassCommand.enhanceClass(inst);
+                case "7" -> {
+                    return;
+                }
+            }
+        }
     }
 }
